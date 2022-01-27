2658ef074f85dc680644249edd24b88c752281ac
unknown <viakeegan@gmail.com>
2022-01-27 03:18:43 -0500

    update node modules to deploy

---
+++ build/precache-manifest.a086781c3f8b01a77c517e39e014f9d4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3572d81c1cb400114a4a2b6913a0f6",
+    "url": "/20-react-portfolio/index.html"
+  },
+  {
+    "revision": "b6759368776ad40a0c53",
+    "url": "/20-react-portfolio/static/css/main.363ba096.chunk.css"
+  },
+  {
+    "revision": "cff7ee00486063e30c9b",
+    "url": "/20-react-portfolio/static/js/2.c522362d.chunk.js"
+  },
+  {
+    "revision": "00330752ade665012574",
+    "url": "/20-react-portfolio/static/js/3.9881a070.chunk.js"
+  },
+  {
+    "revision": "b6759368776ad40a0c53",
+    "url": "/20-react-portfolio/static/js/main.68851213.chunk.js"
+  },
+  {
+    "revision": "4b9291885b68d4859724",
+    "url": "/20-react-portfolio/static/js/runtime~main.43772b20.js"
+  },
+  {
+    "revision": "2ab3e92d218e92f54e027c6a42852f4d",
+    "url": "/20-react-portfolio/static/media/_DSC9074.2ab3e92d.jpg"
+  },
+  {
+    "revision": "12d7dda20fd7b8495d071127a019f0e8",
+    "url": "/20-react-portfolio/static/media/daily-planner.12d7dda2.png"
+  },
+  {
+    "revision": "39a6a554d6e4c36ee4e307c7ff811c60",
+    "url": "/20-react-portfolio/static/media/github-10-512.39a6a554.png"
+  },
+  {
+    "revision": "fb595be1a0e06bf7eca2b10f3b0e7072",
+    "url": "/20-react-portfolio/static/media/linkedin-logo-white.fb595be1.png"
+  },
+  {
+    "revision": "945a5e475f100a87f42cb218126e3a6d",
+    "url": "/20-react-portfolio/static/media/nodesdemo.945a5e47.png"
+  },
+  {
+    "revision": "829708a6b6a95f93ec53a368a0e2b00d",
+    "url": "/20-react-portfolio/static/media/note-taker.829708a6.png"
+  },
+  {
+    "revision": "b870d8ebafd1fc17eec1d49a5e3f1c02",
+    "url": "/20-react-portfolio/static/media/run-buddy.b870d8eb.png"
+  },
+  {
+    "revision": "f920fff32f9a9081f4646195dbdaba85",
+    "url": "/20-react-portfolio/static/media/screenshot.f920fff3.png"
+  },
+  {
+    "revision": "caaa8a7ad50dafda9088f5ebc629eae7",
+    "url": "/20-react-portfolio/static/media/stackoverflow-512.caaa8a7a.png"
+  },
+  {
+    "revision": "efad3568d3440ab8ec3c1d753f714e48",
+    "url": "/20-react-portfolio/static/media/weather-dashboard.efad3568.png"
+  }
+]);
