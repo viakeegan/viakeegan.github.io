bfb26faa9387adadd69a853f3ed364d5e0e079da
unknown <viakeegan@gmail.com>
2022-01-27 01:59:45 -0500

    start app.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import Header from './components/Header';
+import About from "./components/About";
+import Portfolio from './components/Portfolio';
+import ContactForm from "./components/Contact";
+import Resume from './components/Resume';
+import Footer from "./components/Footer";
+import { capitalizeFirstLetter } from "./utils/helpers";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
 
 export default App;
